93b55dfbcb35784bf1b9de5d7eacd5baebecb8cd
Ivan Zhur <vzhur98@gmail.com>
2018-09-08 01:10:18 +0300

    Add photo, dates, route splitting and start cities to Selena

--- backend/search/scr/spiders/spider_ptv.py
+++ backend/search/scr/spiders/spider_ptv.py
@@ -6,6 +6,8 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from database import selena as selena_db
 from multiprocessing import Process, Queue
+from bs4 import BeautifulSoup
+from scrapy import Selector
 
 
 def currency_site_to_code(string):
@@ -16,31 +18,29 @@ def currency_site_to_code(string):
     return 'UNKNOWN'
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "selena"
+class PtvSpider(scrapy.Spider):
+    name = "ptv"
     start_urls = [
-        'http://selena-tour.com.ua/vse_tury/avtobusnie_tury'
+        'http://ptv.com.ua/type/bus-trip/'
     ]
 
     def parse(self, response):
-        for item in response.css('div.block_item'):
+        soup = BeautifulSoup(response.body)
+        resp = Selector(soup.prettify())
+        a = 0
+        for item in resp.css('li.article'):
+            print(len(response.css('li.article')))
             tour = dict()
-            tour['title'] = item.css('div')[4].xpath('normalize-space()').extract_first()
+            a += 1
+            tour['id'] = a
+            tour['title'] = item.css('div.preview')[3].extract()
+            yield tour
+            continue
             tour['price'] = item.css('div')[5].xpath('text()')[1].extract().strip()
             # currency
             currency_site = item.css('div')[5].css('span::text')[1].extract().strip()
             tour['currency'] = currency_site_to_code(currency_site)
             route_str = tour['route_str'] = item.css('tr')[0].css('td::text')[1].extract().strip()
-
-            # image
-            style_str = item.css('div::attr(style)').extract_first()
-            backgrounds = re.findall('background-image:[ ]*url\(.*\)', style_str)
-            if len(backgrounds) > 0:
-                bcg = backgrounds[0]
-                bcg = bcg[bcg.find('url(') + 4: len(bcg)-1]
-                tour['image'] = "http://selena-tour.com.ua" + bcg
-
-            # days count
             tour['days'] = int(item.css('tr')[1].css('td::text')[1].extract().strip())
 
             # split route
@@ -49,7 +49,7 @@ class QuotesSpider(scrapy.Spider):
             tour['places'] = places
 
             # link to tour page
-            link_attr = item.css('div::attr(onclick)').extract()[0]
+            link_attr = item.css("div::attr(onclick)").extract()[0]
             link = 'http://selena-tour.com.ua' + link_attr[link_attr.find("'") + 1: link_attr.rfind("'")]
 
             # print(tour)
@@ -62,16 +62,8 @@ class QuotesSpider(scrapy.Spider):
 
         # start cities
         info_table = response.css('table.tbl_tour_info')
-        cities_str = info_table.css('tr')[3].css('td::text').extract()[1].strip()
+        cities_str = info_table.css('tr')[2].css('td::text')[1].strip()
         tour['start_places'] = [c.strip() for c in cities_str.split(',')]
-
-        # dates
-        dates = info_table.css('tr')[0].css('select.inp_t')[0].css('option::text').extract()
-        d = list()
-        for s in dates:
-            for ss in re.findall('([0-9]{2}[-|.][0-9]{2}[-|.][0-9]{4})', s):  # dd.mm.yyyy
-                d.append(ss)
-        tour['dates'] = d
 
         # description
         tour['description'] = list()
@@ -102,7 +94,7 @@ class QuotesSpider(scrapy.Spider):
 def run_spider_process(q):
     try:
         runner = CrawlerRunner(get_project_settings())
-        deferred = runner.crawl(QuotesSpider)
+        deferred = runner.crawl(PtvSpider)
         deferred.addBoth(lambda _: reactor.stop())
         reactor.run()
         q.put(None)
