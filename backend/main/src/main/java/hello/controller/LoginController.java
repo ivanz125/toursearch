73254f90ab75c5df954a443640a4900ea0409405
Ivan Zhur <vzhur98@gmail.com>
2018-09-08 21:49:22 +0300

    Connect to MySQL DB, login

--- backend/main/src/main/java/hello/controller/LoginController.java
+++ backend/main/src/main/java/hello/controller/LoginController.java
@@ -1,13 +1,27 @@
 package hello.controller;
 
+import hello.model.User;
+import hello.repository.UserRepository;
+import hello.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public LoginController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/login")
     public String page(Model model) {
@@ -15,7 +29,26 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginUser(@RequestParam(name="email") String email, @RequestParam(name="password") String password) {
-        return "";
+    public @ResponseBody String loginUser(@RequestParam(name="email") String email,
+                                          @RequestParam(name="password") String password,
+                                          HttpSession session) {
+        password = Utils.md5(password);
+        if (password == null) return "Exception";
+
+        for (User user : userRepository.findAll()) {
+            if (email.equals(user.getEmail())) {
+                // Success
+                System.out.println(user.getEmail() + " : " + user.getPassword());
+                System.out.println(user.getPassword());
+                System.out.println(password);
+                if (password.equals(user.getPassword())) {
+                    session.setAttribute("user", user);
+                    session.setMaxInactiveInterval(10);
+                    return "OK";
+                }
+                return "Wrong password";
+            }
+        }
+        return "User not exists";
     }
 }
---
+++ backend/main/src/main/java/hello/util/Utils.java
@@ -0,0 +1,21 @@
+package hello.util;
+
+import java.security.MessageDigest;
+
+public class Utils {
+
+    public static String md5(String s) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = md5.digest(s.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < bytes.length; ++i) {
+                sb.append(Integer.toHexString((bytes[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        }
+        catch (Exception ex) {
+            return null;
+        }
+    }
+}
